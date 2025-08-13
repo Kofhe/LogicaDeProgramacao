32b75bf6e6f5bbd47879061e3f7ea2172d8ae6ca
Andreia Dias <andreiadias2311@gmail.com>
2025-08-13 18:30:20 -0300

    Create Quem_está_aí.py

---
+++ Quem_está_aí.py
@@ -0,0 +1,13 @@
+pergunta = "Quem está aí? "
+
+resposta = input(pergunta)
+
+mensagem_saida = f"Olá, {resposta}! =)"
+
+print(mensagem_saida)
+
+if resposta =="":
+    resposta = "pessoa"
+
+    mensagem_saida = f"Olá, {resposta}!"
+    print (mensagem_saida)
